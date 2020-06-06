5d1d945b51918d9d1395c649fed8b989ed090bab
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-06 20:12:08 +0900

    practie

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+      else
+        format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
+  end
+end
